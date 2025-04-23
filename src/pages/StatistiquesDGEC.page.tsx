d25b137cafee7d7566e1354d4b19abfd899fa0a0
Hubert MONCENIS <hubmoncenis@gmail.com>
2025-04-23 16:24:31 +0200

    🎨 Export default component

--- src/pages/StatistiquesDGEC.page.tsx
+++ src/pages/StatistiquesDGEC.page.tsx
@@ -1,28 +1,30 @@
 "use client";
 
-import React, { FC } from "react";
+import React from "react";
 import IframeResizer from "@iframe-resizer/react";
 
 type StatistiquesDGECPageProps = {
   dashboardIframeUrl: string;
 };
 
-export const StatistiquesDGECPage: FC<StatistiquesDGECPageProps> = ({
+export default function StatistiquesDGECPage({
   dashboardIframeUrl,
-}) => (
-  <>
-    <h1
-      className={`mt-15 text-3xl text-center leading-8 font-bold text-[#000091]`}
-    >
-      Potentiel en chiffres - Dashboard DGEC
-    </h1>
-    <div className="my-10">
-      <IframeResizer
-        license="GPLv3"
-        src={dashboardIframeUrl}
-        style={{ width: "100%", height: "auto" }}
-        className="mt-15"
-      />
-    </div>
-  </>
-);
+}: StatistiquesDGECPageProps) {
+  return (
+    <>
+      <h1
+        className={`mt-15 text-3xl text-center leading-8 font-bold text-[#000091]`}
+      >
+        Potentiel en chiffres - Dashboard DGEC
+      </h1>
+      <div className="my-10">
+        <IframeResizer
+          license="GPLv3"
+          src={dashboardIframeUrl}
+          style={{ width: "100%", height: "auto" }}
+          className="mt-15"
+        />
+      </div>
+    </>
+  );
+}
